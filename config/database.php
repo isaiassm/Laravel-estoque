06bdd69cf62a862bc1a475bc425d09ad3783437f
isaiassm <isaias1113@outlook.com>
2018-11-29 21:11:37 -0200

    [UPDATE] Configuração de banco de dados mysql

--- config/database.php
+++ config/database.php
@@ -42,10 +42,10 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
+            'database' => env('DB_DATABASE', 'estoque_laravel'),
+            'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
