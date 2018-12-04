4773f045c86f27bf903fe79cf37b7ab66d5e736a
isaiassm <isaias1113@outlook.com>
2018-12-04 16:10:22 -0200

    [ UPDATE ] configuracao de servidor ainda com erro

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -5,11 +5,10 @@ use Illuminate\Support\Facades\DB;
 class ProdutoController extends Controller {
    
     public function lista(){
-        $html = '<h1>Listagem de produtos</h1>';
-
-        $produtos = DB::select('select * from produtos');
-       dd($produtos);
-
+       
+     $html = '<h1>Listagem de produtos</h1>';
+      $produtos = DB::select('select * from produtos');
+      
        foreach($produtos as $p){
         $html .= "<br /> Nome: " . $p->nome;
        }
--- config/database.php
+++ config/database.php
@@ -42,7 +42,7 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'localhost'),
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'estoque_laravel'),
             'username' => env('DB_USERNAME', 'root'),
