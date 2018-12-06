238511925550184f79ee29fdac434db3d08e33a8
isaiassm <isaias1113@outlook.com>
2018-12-05 23:23:15 -0200

    [UPDATE] Arquivo env e database configurado, Conexao com o banco funcionando

--- config/database.php
+++ config/database.php
@@ -44,7 +44,7 @@ return [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'estoque_laravel'),
+            'database' => env('DB_DATABASE', 'estoque_laraveldb'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
