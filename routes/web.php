3ab8415c1ebd22e6d2263c20595d5a58a9c601b8
isaiassm <isaias1113@outlook.com>
2018-12-12 22:38:44 -0200

    [ADD] Incluindo rota de login para inicio de contrucao da tela de login

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,5 @@ Route::get('/produtos/mostra/{id}', 'ProdutoController@mostra');
 Route::get('/produtos/remove/{id}', 'ProdutoController@remove');
 Route::get('/produtos/novo', 'ProdutoController@novo');
 Route::post('/produtos/adiciona', 'ProdutoController@adiciona');
+
+Route::get('/login', 'LoginoController@form');
